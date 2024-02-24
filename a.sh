00ffbecc9aa793c00ae946b83163a00967129735
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-02-25 03:03:46 +0800

    e

--- a.sh
+++ a.sh
@@ -2,7 +2,7 @@
 
 rm -rf h870/* h872/* us997/* 
 rm release/*.zip
-crave pull out/target/product/*/*.zip out/target/product/*/recovery.img out/target/product/*/*.json out/target/product/*/changelog_*.txt 
+crave pull out/target/product/*/changelog_*.txt 
 mv h870/recovery.img h870/recoveryh870.img
 mv h872/recovery.img h872/recoveryh872.img
 mv us997/recovery.img us997/recoveryus997.img
