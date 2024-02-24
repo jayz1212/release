541c6ab5aee88022fc172b51f014870d2b15f2ac
jayz1212 <160797500+jayz1212@users.noreply.github.com>
2024-02-25 03:02:55 +0800

    f

--- a.sh
+++ a.sh
@@ -6,6 +6,8 @@ crave pull out/target/product/*/*.zip out/target/product/*/recovery.img out/targ
 mv h870/recovery.img h870/recoveryh870.img
 mv h872/recovery.img h872/recoveryh872.img
 mv us997/recovery.img us997/recoveryus997.img
-mv h870/* h872/* us997/* ./release/ 
+mv h870/* h872/* us997/* ./release 
 cd release
-./m.sh
+ls
+chmod u+x m.sh
+. m.sh
